e21f87bd896f4c3090d1b1c1f8b064267635afa1
vzone <857414521@qq.com>
2019-11-28 09:17:30 +0800

    添加全局捕获异常的注释

--- src/main/java/com/xhg/excepetion/GlobalException.java
+++ src/main/java/com/xhg/excepetion/GlobalException.java
@@ -1,21 +1,22 @@
 package com.xhg.excepetion;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xhg.vo.JsonResult;
 
-@ControllerAdvice //原理是使用AOP对Controller控制器进行增强（前置增强、后置增强、环绕增强，AOP原理请自行查阅）
+//@ControllerAdvice 可以配置basePackage下的所有controller
+//原理是使用AOP对Controller控制器进行增强（前置增强、后置增强、环绕增强，AOP原理请自行查阅）
+
+@ControllerAdvice
 public class GlobalException {
 	
-	@ExceptionHandler(RuntimeException.class)
+	@ExceptionHandler(RuntimeException.class) //@ExceptionHandler，可以处理异常
+											  //配合@ControllerAdvice 便可以处理全局异常
 	@ResponseBody
 	public JsonResult resultException(Exception e){
-		
+		//e.printStackTrace();
 		return new JsonResult(e);
 		
 	}
