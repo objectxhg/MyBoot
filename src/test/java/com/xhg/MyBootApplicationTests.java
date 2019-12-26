b296151f43a1f4a5804f1ca147b94f25843d3118
objectxhg <857414521@qq.com>
2019-12-26 20:23:54 +0800

    fastjson Junit

--- src/test/java/com/xhg/MyBootApplicationTests.java
+++ src/test/java/com/xhg/MyBootApplicationTests.java
@@ -1,22 +1,45 @@
 package com.xhg;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.xhg.utils.RedisUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.xhg.pojo.User;
 
 @SpringBootTest
 class MyBootApplicationTests {
 	
 	
-	@Autowired
-	private RedisUtil redisUtil;
-	
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
 		
-		System.out.println(redisUtil.get("name"));
+		test1();
+		
+	}
+	public static void test1() {
+		
+		User user = new User();
+		user.setId(1);
+		user.setMobile("15573679072");
+		user.setPwd("123456");
+		user.setAddress("湖南长沙");
+		user.setBirthday("0816");
+		
+		String userStr = JSON.toJSONString(user,
+				SerializerFeature.PrettyFormat,
+				//单引号输出
+				SerializerFeature.UseSingleQuotes,
+				//输出为空的字段
+				SerializerFeature.WriteNullStringAsEmpty);
+		
+		System.out.println(userStr);
+		
+		String jsonString = "{\"birthday\":"
+				+ "\"2018-08-17 14:38:38\",\"id\":11,\"address\":\"湖南常德\"}";
+	
+		User userJson = JSON.parseObject(jsonString, User.class);
+		
+		System.out.println(userJson);
 	}
 
 }
