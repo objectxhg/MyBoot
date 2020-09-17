6d9be05354c0c9dcac4441c87c4802adbe31b94a
objectxhg <857414521@qq.com>
2020-09-17 19:39:34 +0800

    同上

--- src/main/java/com/xhg/config/rabbitMQ/Receiver.java
+++ src/main/java/com/xhg/config/rabbitMQ/Receiver.java
@@ -44,7 +44,7 @@ public class Receiver {
             //模拟处理消息产生异常后进入死信队列进行消费
             //int i = 1/0;
 
-            Integer state = userService.incrUserIntegral(user);
+            Integer state = userService.incrUserIntegral(user.getId());
             if(state == 1){
                 System.out.println("【ququDemo正常队列】-----> 消费成功 购物积分已增加");
                 // 手动签收消息已消费
