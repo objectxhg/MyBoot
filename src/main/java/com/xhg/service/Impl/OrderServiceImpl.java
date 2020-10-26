ed88dc0db799948212b6d9520abca213af3aef48
objectxhg <857414521@qq.com>
2020-10-26 14:54:02 +0800

    10-26 替换数据库序列ID，使用雪花算法

--- src/main/java/com/xhg/service/Impl/OrderServiceImpl.java
+++ src/main/java/com/xhg/service/Impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.xhg.mapper.OrderMapper;
 import com.xhg.mapper.SysUserMapper;
 import com.xhg.pojo.Order;
 import com.xhg.service.OrderService;
+import com.xhg.utils.SnowflakeIdWorker;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -34,11 +35,15 @@ public class OrderServiceImpl implements OrderService {
         /**
          * 时间戳唯一工具类ID ：order.setOrderId(SnowflakeUtil.getSnowflakeID()); 缺点：18位字符太长了，浪费而且也不好看
          *
-         * 利用redis得单线程原子性存一个数值每次插入incr 来生成序列
+         * 利用redis得单线程原子性存一个数值每次插入incr 来生成序列，需要做持久化AOF 和 RDB，redis挂掉重启时，由于incr命令的特殊性，恢复数据慢 （具体原因不记得了）
          *
-         * 数据库序列 mycat配置（当前使用，建议）
+         * 数据库序列 mycat配置（当前使用）缺点（已使用mycat配置 默认不插入id 就是使用数据库序列id） 使用数据库序列的DB可能会宕机，无发抗住高并发
+         * insert
+         * id 写为：next value for MYCATSEQ_ORDERS
+         *
+         * 雪花算法
          */
-
+        order.setOrderId(SnowflakeIdWorker.generateId());
         order.setOrderUpdateTime(new Date());
         return orderMapper.createOrder(order);
     }
---
+++ src/main/java/com/xhg/utils/demo/JKtimeDemo.java
@@ -0,0 +1,40 @@
+package com.xhg.utils.demo;
+
+/**
+ * @Author xiaoh
+ * @create 2020/10/21 15:49
+ */
+public class JKtimeDemo {
+
+        public static void main(String[] args) {
+
+        int[] arr = {2,15,17,7};
+        System.out.println(twoSum(arr,9));
+
+
+
+    }
+
+
+    public static int JieCheng(int n){
+         if(n == 1){
+             return 1;
+         }else {
+             return n*JieCheng(n-1);
+         }
+     }
+
+    public static String twoSum(int[] arr, int target){
+
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 1; j < arr.length; j++){
+                if(arr[i] + arr[j] == target){
+                    return i +","+ j;
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
