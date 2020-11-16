393643c2f3c21f08b964bd6586d87f316a2e3f22
objectxhg <857414521@qq.com>
2020-11-16 23:58:11 +0800

    11-16 redis pipeline测试： 未完成

--- src/test/java/com/xhg/DemoTest.java
+++ src/test/java/com/xhg/DemoTest.java
@@ -1,10 +1,19 @@
 package com.xhg;
 
+
+import com.xhg.utils.RedisUtil;
 import com.xhg.utils.SnowflakeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 /**
  * @Author xiaoh
@@ -13,6 +22,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {MyBootApplication.class})
 public class DemoTest {
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     public void SnowflakeTest(){
@@ -23,6 +38,14 @@ public class DemoTest {
     @Test
     public void volatileTestDemo(){
 
+        String valueStr = "idea";
+        redisTemplate.executePipelined(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.sAdd("good".getBytes(), "idea".toString().getBytes());
+                return null;
+            }
+        });
     }
 
 }
