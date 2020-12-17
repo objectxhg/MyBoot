2482cf8f740039af199d68783577997446f8ae10
objectxhg <857414521@qq.com>
2020-12-17 11:35:10 +0800

    12-17 由于srpingboot内置tomcat 版本太高对http请求中特定字符串限制问题

---
+++ src/main/java/com/xhg/config/web/WebServerConfig.java
@@ -0,0 +1,29 @@
+package com.xhg.config.web;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author xiaoh
+ * @create 2020/12/17 10:58
+ */
+@Configuration
+public class WebServerConfig {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        });
+        return factory;
+    }
+
+}
